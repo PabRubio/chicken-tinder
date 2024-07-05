7101a449c31d5a169a22d7669d76effac51c3a90
PabRubio <pablo.rubio.punal@gmail.com>
2024-07-05 11:15:01 +0200

    Create Django app

--- chicken_tinder/settings.py
+++ chicken_tinder/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps.
+    'users',
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
