dab5550922994093edbc91f4775ca7067e585909
PabRubio <pablo.rubio.punal@gmail.com>
2024-07-19 12:13:02 +0200

    Setup profiles app

--- chicken_tinder/settings.py
+++ chicken_tinder/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps.
     'users',
+    'profiles',
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ profiles/urls.py
@@ -0,0 +1,10 @@
+"""Defines URL patterns for profiles."""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'profiles'
+urlpatterns = [
+    
+]
