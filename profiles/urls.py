e626bd78ccd0948cb3e38fbd59058ee474714a98
Pablo Rubio <pablo.rubio.punal@gmail.com>
2024-09-11 22:16:20 +0200

    Update urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,16 @@
 """Defines URL patterns for profiles."""
 
 from django.urls import path
-
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import edit_profile, view_profile
 
 app_name = 'profiles'
 urlpatterns = [
-    
+    path('settings/', edit_profile, name='settings'),
+    path('profile/', view_profile, name='profile'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
