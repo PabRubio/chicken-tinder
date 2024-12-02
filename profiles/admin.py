1178dd20385063fcef4bff7be94b8d8e38da9fc6
PabRubio <pablo.rubio.punal@gmail.com>
2024-12-02 17:19:37 +0200

    Register admin models

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile, Image
 
-# Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'breed', 'age', 'location', 'egg_production',
+                    'temperament', 'foraging_skill', 'favorite_food')
+    list_filter = ('breed', 'egg_production', 'temperament',
+                   'foraging_skill', 'favorite_food')
+    search_fields = ('name', 'breed', 'location', 'bio')
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'image')
+    search_fields = ('profile__name',)
