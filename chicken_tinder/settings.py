488608fa79641a48dd1d71d5ab42b346bd1eecd3
PabRubio <pablo.rubio.punal@gmail.com>
2024-12-02 17:26:37 +0200

    Create custom 404.html

--- chicken_tinder/settings.py
+++ chicken_tinder/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'chicken_tinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
