b7c89bdd9f2f3c1e69e71bb58822d03d6324dfd3
PabRubio <pablo.rubio.punal@gmail.com>
2024-12-04 17:54:37 +0200

    Set DEBUG False

--- chicken_tinder/settings.py
+++ chicken_tinder/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%bzc=z@2ow^g7syr)o)yqwi$%ovgf%+*b!jbalsbk3q3!ogj6j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'chickentinder.lol']
 
 
 # Application definition
